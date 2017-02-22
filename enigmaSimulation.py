7d99b0eb8ae4c1bb794dfc2c1312d9851491b50c
Maddie Zug <madelinezug@gmail.com>
2017-02-21 16:54:08 -0800

    added initial files

---
+++ enigmaSimulation.py
@@ -0,0 +1,19 @@
+#
+# Enigma Simulation 
+# Maddie Zug
+# Cryptography
+# Harvey Mudd College
+#
+
+
+def main():
+	f = open('enigma.txt', 'r')
+	plaintext = f.read()
+	print(plaintext)
+	rotor1 = {}
+	rotor2 = {}
+	rotor3 = {}
+	rotor4 = {}
+
+if __name__ == '__main__':
+	main()
